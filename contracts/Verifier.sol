891d99cef63551ca313c1f1de1e9afac6c5fedcc
eversoluna <phan.powray@outlook.com>
2020-09-28 09:36:22 +0700

    update verifier

--- contracts/Verifier.sol
+++ contracts/Verifier.sol
@@ -5,7 +5,6 @@ contract Verifier {
     // Placeholder for actual zk-proof verification
     function verify(bytes memory proof) public pure returns (bool) {
         // Simulated zk-proof verification: check proof length (for demonstration)
-        // In production, replace with actual zk-SNARK/zk-STARK verification logic
         if (proof.length == 128) {
             return true;
         }
